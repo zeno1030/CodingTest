f2f431f9728ec73691d3159c551ce3996e2dd8db
Kwon Ji Hwan <zeno1030@naver.com>
2025-01-24 09:38:22 +0900

    solved

---
+++ programmers/network.py
@@ -0,0 +1,18 @@
+
+def solution(n, computers):
+    answer = 0
+    visited = [0] * n
+
+
+    def dfs(x):
+        print(x)
+        visited[x] = 1
+        for key, value in enumerate(computers[x]):
+            if value and visited[key] == 0:
+                dfs(key)
+
+    for i in range(n):
+        if visited[i] == 0:
+            dfs(i)
+            answer += 1
+    return answer
