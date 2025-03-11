626a2a626ab60395f804bd8b7784b14216f9d7d2
Kwon Ji Hwan <zeno1030@naver.com>
2025-03-11 17:01:16 +0900

    solved

--- back_IT/2293.py
+++ back_IT/2293.py
@@ -3,8 +3,16 @@
 
 if __name__ == '__main__':
     n, k = map(int, input().split())
-    coin = []
 
-    for _ in range(n):
-        coin.append(int(input()))
+    coins = []
+    for i in range(n):
+        coins.append(int(input()))
+    coins.sort()
+
+    DP = [0] * (k + 1)
+    DP[0] = 1
+    for c in coins:
+        for i in range(c, k + 1):
+            DP[i] += DP[i - c]
+    print(DP[k])
 
