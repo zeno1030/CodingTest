4706ca3edcae3175165066e6f014e7e4ca25d2a8
Kwon Ji Hwan <zeno1030@naver.com>
2025-03-13 14:35:57 +0900

    15683 푸는 중

---
+++ back_IT/15683.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+
+
