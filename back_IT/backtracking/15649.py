0939ae1616c333d2289dcaa2eec5dba0cba67c17
Kwon Ji Hwan <zeno1030@naver.com>
2025-04-04 16:39:56 +0900

    solved

---
+++ back_IT/backtracking/15649.py
@@ -0,0 +1,20 @@
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs()
+        s.pop()
+
+        visited[i] = False
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    s = []
+    visited = [False] * (n + 1)
+
+    dfs()
---
+++ back_IT/bfs/4179.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    matrix = [list((input().strip())) for _ in range(n)]
+
+    print(matrix)
