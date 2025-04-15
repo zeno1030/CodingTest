81ff943f528f85cd4c77b90cfac09d168a90a3c1
Kwon Ji Hwan <zeno1030@naver.com>
2025-04-15 14:52:46 +0900

    solved

---
+++ back_IT/simulation/7568.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+
+    for i in arr:
+        rank = 1
+        for j in arr:
+            if i[0] > j[0] and i[1] > j[1]:
+                rank += 1
+
+        print(rank, end=" ")
